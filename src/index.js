b457022ef966798edfeb44b57b5805698ef4bf7f
Lubenets Slava <wenzelua@gmail.com>
2019-06-19 15:53:50 +0300

    id:86246 feat(save-as): add save as button

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const app = {
     document.querySelector('.controls__form__entity-id').addEventListener('submit', this.handleOpenTemplate.bind(this));
     document.querySelector('.controls__form__saved-item-id').addEventListener('submit', this.handleOpenSavedItem.bind(this));
     document.querySelector('.controls__save-button').addEventListener('click', this.handleSave.bind(this));
+    document.querySelector('.controls__save-as-button').addEventListener('click', this.handleSaveAs.bind(this));
   },
 
   handleLoad({ id, type }) {
@@ -33,9 +34,16 @@ const app = {
     this.brandgilityEmbeddedApi.emit('save');
   },
 
+  handleSaveAs() {
+    this.brandgilityEmbeddedApi.emit('saveAs', {
+      name: 'new item',
+      comments: 'comments here',
+    });
+  },
+
   handleOpenTemplate(event) {
     const templateId = document.querySelector('.controls__entity-id-input').value.trim();
-    const url = `${env.brandgilityUrl}/embedded-template-configure/new/${templateId}?action=START_TEMPLATE_CONFIGURATION&eid=${templateId}&embedded=true`;
+    const url = `${env.brandgilityUrl}/embedded-template-configure/new/${templateId}?action=DRAFT_TEMPLATE_CONFIGURATION_START&eid=${templateId}&embedded=true`;
 
     event.preventDefault();
     this.openIframe(url);
@@ -43,7 +51,7 @@ const app = {
 
   handleOpenSavedItem(event) {
     const savedItemId = document.querySelector('.controls__saved-item-id-input').value.trim();
-    const url = `${env.brandgilityUrl}/embedded-template-configure/edit/${savedItemId}?action=SAVED_ITEM&eid=${savedItemId}&embedded=true`;
+    const url = `${env.brandgilityUrl}/embedded-template-configure/edit/${savedItemId}?action=DRAFT_TEMPLATE_CONFIGURATION_EDIT&eid=${savedItemId}&embedded=true`;
 
     event.preventDefault();
     this.openIframe(url);
